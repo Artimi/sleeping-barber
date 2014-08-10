5cbe0a0ec7389564bf5369a8a5ef89ae76e51e33
Petr Šebek <petrsebek1@gmail.com>
2014-08-10 21:14:59 +0200

    Barber, customer function in goroutines

--- barber.go
+++ barber.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func barber() {
+	fmt.Printf("I am a Barber.\n")
+}
+
+func customer() {
+	fmt.Printf("I am a Customer.\n")
+}
+
 func main() {
 	fmt.Printf("Hello, Barber.\n")
+
+	go barber()
+	go customer()
+
+	var input string
+	fmt.Scanln(&input)
 }
