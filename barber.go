676760850d77f0b714622ab2268c00839fe3f8f9
Petr Šebek <petrsebek1@gmail.com>
2014-08-10 21:37:11 +0200

    Added reading parameters from command line

--- barber.go
+++ barber.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+type Params struct {
+	Customers      int
+	Seats          int
+	Customer_delay float64
+	Barber_delay   float64
+}
 
 func barber() {
 	fmt.Printf("I am a Barber.\n")
@@ -10,7 +20,20 @@ func customer() {
 	fmt.Printf("I am a Customer.\n")
 }
 
+func read_params() *Params {
+	var params *Params = new(Params)
+	flag.IntVar(&params.Customers, "customers", 10, "number of customers to come to barber shop")
+	flag.IntVar(&params.Seats, "seats", 3, "number of seats in barber shop")
+	flag.Float64Var(&params.Customer_delay, "customers_delay", 100, "mean value of time of customers incoming to shop")
+	flag.Float64Var(&params.Barber_delay, "barber_delay", 150, "mean value of time of barbers work on customer")
+
+	flag.Parse()
+	return params
+}
+
 func main() {
+	params := read_params()
+	fmt.Printf("%#v\n", params)
 	fmt.Printf("Hello, Barber.\n")
 
 	go barber()
