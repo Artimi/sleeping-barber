29b58b41840d97e3c42f403ec1a84061b3dedb4c
Petr Šebek <petrsebek1@gmail.com>
2014-08-12 00:40:41 +0200

    Customer reentering shop if full after some delay

--- barber.go
+++ barber.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Params struct {
-	Customers      int
-	Seats          int
-	Customer_delay int
-	Barber_delay   int
+	Customers             int
+	Seats                 int
+	Customer_delay        int
+	Customer_return_delay int
+	Barber_delay          int
 }
 
 func serve_customer(customer_id int, delay int, customer_served chan int) {
@@ -33,17 +34,22 @@ func barber(delay int, seats chan int, customer_served chan int) {
 	}
 }
 
-func customer(id int, seats chan int, customer_served chan int) {
-	select {
-	case seats <- id:
-		fmt.Printf("CUSTOMER %d: Entering barber shop.\n", id)
-		finished_customer := -1
-		for finished_customer != id {
-			finished_customer = <-customer_served
+func customer(id int, return_delay int, seats chan int, customer_served chan int) {
+	var serviced bool = false
+	for serviced != true {
+		select {
+		case seats <- id:
+			fmt.Printf("CUSTOMER %d: Entering barber shop.\n", id)
+			finished_customer := -1
+			for finished_customer != id {
+				finished_customer = <-customer_served
+			}
+			fmt.Printf("CUSTOMER %d: Leaving barber shop happy.\n", id)
+			serviced = true
+		default:
+			fmt.Printf("CUSTOMER %d: Leaving barber shop unserviced.\n", id)
+			time.Sleep(time.Duration(return_delay) * time.Millisecond)
 		}
-		fmt.Printf("CUSTOMER %d: Leaving barber shop happy.\n", id)
-	default:
-		fmt.Printf("CUSTOMER %d: Leaving barber shop unserviced.\n", id)
 	}
 }
 
@@ -51,8 +57,9 @@ func read_params() *Params {
 	var params *Params = new(Params)
 	flag.IntVar(&params.Customers, "customers", 10, "number of customers to come to barber shop")
 	flag.IntVar(&params.Seats, "seats", 3, "number of seats in barber shop")
-	flag.IntVar(&params.Customer_delay, "customers_delay", 5, "mean value of time of customers incoming to shop")
-	flag.IntVar(&params.Barber_delay, "barber_delay", 15, "mean value of time of barbers work on customer")
+	flag.IntVar(&params.Customer_delay, "customers_delay", 5, "time between customers incoming to shop")
+	flag.IntVar(&params.Customer_return_delay, "customers_return_delay", 20, "time between customers' retry if the barber shop is full")
+	flag.IntVar(&params.Barber_delay, "barber_delay", 15, "time of barber working on customer")
 
 	flag.Parse()
 	return params
@@ -66,7 +73,7 @@ func main() {
 
 	go barber(params.Barber_delay, seats, customer_served)
 	for id := 0; id < params.Customers; id++ {
-		go customer(id, seats, customer_served)
+		go customer(id, params.Customer_return_delay, seats, customer_served)
 		time.Sleep(time.Duration(params.Customer_delay) * time.Millisecond)
 	}
 
